87a4399a1b00071d130a774d4a4e987840ee94d5
Developer <maitryweblabs@gmail.com>
2025-04-25 18:28:54 +0530

    Commented systemToolRoutes code cause it's pending so throws error

--- src/configs/routes.config/systemToolsRoutes.ts
+++ src/configs/routes.config/systemToolsRoutes.ts
@@ -3,25 +3,25 @@ import { SYSTEM_TOOLS_PREFIX_PATH } from '@/constants/route.constant';
 import { ADMIN } from '@/constants/roles.constant';
 import type { Routes } from '@/@types/routes';
 
-const systemToolsRoutes: Routes = [
-    {
-        key: 'systemTools.bugReport',
-        path: `${SYSTEM_TOOLS_PREFIX_PATH}/bug-report`,
-        component: lazy(() => import('@/views/system-tools/BugReport')),
-        authority: [ADMIN],
-        meta: {
-            pageContainerType: 'contained',
-        },
-    },
-    {
-        key: 'systemTools.changeLog',
-        path: `${SYSTEM_TOOLS_PREFIX_PATH}/change-log`,
-        component: lazy(() => import('@/views/systemTools/ChangeLog')),
-        authority: [ADMIN],
-        meta: {
-            pageContainerType: 'contained',
-        },
-    },
-];
+// const systemToolsRoutes: Routes = [
+//     {
+//         key: 'systemTools.bugReport',
+//         path: `${SYSTEM_TOOLS_PREFIX_PATH}/bug-report`,
+//         component: lazy(() => import('@/views/system-tools/BugReport')),
+//         authority: [ADMIN],
+//         meta: {
+//             pageContainerType: 'contained',
+//         },
+//     },
+//     {
+//         key: 'systemTools.changeLog',
+//         path: `${SYSTEM_TOOLS_PREFIX_PATH}/change-log`,
+//         component: lazy(() => import('@/views/systemTools/ChangeLog')),
+//         authority: [ADMIN],
+//         meta: {
+//             pageContainerType: 'contained',
+//         },
+//     },
+// ];
 
-export default systemToolsRoutes;
+// export default systemToolsRoutes;
