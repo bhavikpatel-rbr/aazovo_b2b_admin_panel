5f3233e2d93feab25a4eecdbe2e2e0052492bdd2
dravyatechnosys <dravyatechnosys@gmail.com>
2025-06-04 17:49:26 +0530

    blog and tranding changes

--- src/views/web-settings/Trending-Carousel/TrendingCarousel.tsx
+++ src/views/web-settings/Trending-Carousel/TrendingCarousel.tsx
@@ -645,7 +645,7 @@ const TrendingCarousel = () => {
 
       <TrendingCarouselSelectedFooter selectedItems={selectedItems} onDeleteSelected={handleDeleteSelected} isDeleting={isDeleting || masterLoadingStatus === "idle"} />
 
-      <Drawer title="Add Trending Carousel" isOpen={isAddDrawerOpen} onClose={closeAddDrawer} onRequestClose={closeAddDrawer} width={600}
+      <Drawer title="Add Trending Carousel" isOpen={isAddDrawerOpen} onClose={closeAddDrawer} onRequestClose={closeAddDrawer} width={480}
         footer={
           <div className="text-right w-full">
             <Button size="sm" className="mr-2" onClick={closeAddDrawer} disabled={isSubmitting} type="button"> Cancel </Button>
@@ -669,7 +669,7 @@ const TrendingCarousel = () => {
                 />
               )}
             />
-            {imagePreviewUrl && (<Avatar src={imagePreviewUrl} size={100} shape="square" className="mt-2 border border-gray-200 dark:border-gray-600" />)}
+            {imagePreviewUrl && (<Avatar src={imagePreviewUrl} className="w-[430px] h-[auto] border p-1 rounded-md mt-2" shape="square" />)}
             <p className="text-xs text-gray-500 dark:text-gray-400 mt-1"> Max file size 5MB. Accepted: .jpg, .png, .webp, .gif </p>
           </FormItem>
           <FormItem label="Link (Optional)" invalid={!!formMethods.formState.errors.links} errorMessage={formMethods.formState.errors.links?.message}>
@@ -678,7 +678,7 @@ const TrendingCarousel = () => {
         </Form>
       </Drawer>
 
-      <Drawer title="Edit Trending Carousel" isOpen={isEditDrawerOpen} onClose={closeEditDrawer} onRequestClose={closeEditDrawer} width={600}
+      <Drawer title="Edit Trending Carousel" isOpen={isEditDrawerOpen} onClose={closeEditDrawer} onRequestClose={closeEditDrawer} width={480}
         footer={
           <div className="text-right w-full">
             <Button size="sm" className="mr-2" onClick={closeEditDrawer} disabled={isSubmitting} type="button"> Cancel </Button>
