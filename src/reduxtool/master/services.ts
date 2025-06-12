bee019adb72355dfc6b0105d70213144f8633240
bhavikpatel-rbr <bhavikrupapara3@gmail.com>
2025-06-12 23:42:07 +0530

    commitunit

--- src/reduxtool/master/services.ts
+++ src/reduxtool/master/services.ts
@@ -72,7 +72,7 @@ export const editUnitAsync = async (unitData: any) => {
   console.log(`${config.apiURL}/master/unit/${unitData?.id}`, { _method: "PUT", name: unitData?.name });
 
   try {
-    const response = await axiosInstance.post(`${config.apiURL}/master/unit/${unitData?.id}`, { _method: "PUT", name: unitData?.name })
+    const response = await axiosInstance.post(`${config.apiURL}/master/unit/${unitData?.id}`, { _method: "PUT", name: unitData?.name, status: unitData?.status })
     return response
   } catch (err) {
     return isAxiosError(err)
@@ -218,7 +218,7 @@ export const editCurrencyAsync = async (unitData: any) => {
   console.log(`${config.apiURL}/master/currency/${unitData?.id}`, { _method: "PUT", currency_symbol: unitData?.name });
 
   try {
-    const response = await axiosInstance.post(`${config.apiURL}/master/currency/${unitData?.id}`, { _method: "PUT", currency_symbol: unitData?.currency_symbol, currency_code: unitData?.currency_code , status:unitData?.status })
+    const response = await axiosInstance.post(`${config.apiURL}/master/currency/${unitData?.id}`, { _method: "PUT", currency_symbol: unitData?.currency_symbol, currency_code: unitData?.currency_code, status: unitData?.status })
     return response
   } catch (err) {
     return isAxiosError(err)
