0c5a006828a26afa59d4efe21654850886469f36
bhavikpatel-rbr <bhavikrupapara3@gmail.com>
2025-06-12 15:09:56 +0530

    Other not show in isde bar in frameless side - Solve

--- src/components/ui/Menu/MenuGroup.tsx
+++ src/components/ui/Menu/MenuGroup.tsx
@@ -4,6 +4,7 @@ import { GroupContextProvider } from './context/groupContext'
 import MenuContext from './context/menuContext'
 import type { CommonProps } from '../@types/common'
 import type { ReactNode } from 'react'
+import { useThemeStore } from '@/store/themeStore'
 
 export interface MenuGroupProps extends CommonProps {
     label: string | ReactNode
@@ -11,16 +12,17 @@ export interface MenuGroupProps extends CommonProps {
 
 const MenuGroup = (props: MenuGroupProps) => {
     const { label, children, className } = props
-
+  const layoutType = useThemeStore((state) => state.layout.type)
     const { sideCollapsed } = useContext(MenuContext)
 
     const menuGroupDefaultClass = 'menu-group'
     const menuGroupClass = classNames(menuGroupDefaultClass, className)
 
+    
     return (
         <div className={menuGroupClass}>
             {label && !sideCollapsed && label != 'Dashboard' && (
-                <div className={classNames('menu-title')}>{label}</div>
+                <div className={classNames('menu-title')} style={{color :layoutType == 'collapsibleSide' ? 'black' :'white'}}>{label}</div>
             )}
             <GroupContextProvider value={null}>
                 <ul>{children}</ul>
