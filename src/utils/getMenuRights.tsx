b373d714c7faa7df9c88b4a70740ef6e489b278b
tejdekiwadiya <workfor.td@gmail.com>
2025-08-04 22:57:31 +0530

    ------------- Company Rights ------------

---
+++ src/utils/getMenuRights.tsx
@@ -0,0 +1,12 @@
+export const getMenuRights = (menu: string) => {
+    const useRights = JSON.parse(localStorage.getItem('@secure:Userpermission')) || {};
+    const rights = useRights[menu] || {};
+
+    return {
+        is_add: rights.is_add || false,
+        is_delete: rights.is_delete || false,
+        is_edit: rights.is_edit || false,
+        is_export: rights.is_export || false,
+        is_view: rights.is_view || false,
+    };
+}
