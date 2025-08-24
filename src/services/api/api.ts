219dfdc772a60c98905e833dce9825f032d2fc8e
tejdekiwadiya <workfor.td@gmail.com>
2025-08-25 00:22:17 +0530

    changes for api call

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -2,7 +2,6 @@ import axios, { AxiosInstance } from "axios"
 
 import { setupInterceptorsTo } from "./setupInterceptor"
 
-import { config } from "../../utils/config"
 
 import { ErrorResponse } from "./SuccessResponse"
 
@@ -22,14 +21,25 @@ const instance: AxiosInstance = axios.create({
 const specificInstance: AxiosInstance = setupInterceptorsTo(instance)
 
 export const isAxiosError = (err: any): ErrorResponse | any => {
+
   if (axios.isAxiosError(err)) {
-    if (err?.response && err?.response?.data) {
-      if (err?.response?.status === 404) return { status: 404, statusText: err?.message } as ErrorResponse
-      return err?.response?.data as ErrorResponse
+    console.log(err?.response?.data?.message == "Unauthenticated.", "errerrerr");
+    if (err?.response?.data?.message == "Unauthenticated.") {
+      // localStorage.clear();
+      // localStorage.setItem("@secure:user", "undefined")
+      // localStorage.setItem("@secure:Userpermission", "undefined")
+      // localStorage.setItem("@secure:UserData", "undefined")
+      // window.location.href = '/';
+    } else {
+
+      if (err?.response && err?.response?.data) {
+        if (err?.response?.status === 404) return { status: 404, statusText: err?.message } as ErrorResponse
+        return err?.response?.data as ErrorResponse
+      }
+      return { status: 401, statusText: err?.message } as ErrorResponse
+      // return { status: 401, statusText: err?.message } as ErrorResponse
     }
-    return { status: 401, statusText: err?.message } as ErrorResponse
   }
-  return { status: 401, statusText: err?.message } as ErrorResponse
 }
 
 export default specificInstance
